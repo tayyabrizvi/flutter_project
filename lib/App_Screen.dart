4d71d285dbf7c908959c2e292d758c46d711ecdd
Muhammad Tayyab <mtayyabrizvi0@gmail.com>
2024-08-25 13:36:15 +0500

    first commit

---
+++ lib/App_Screen.dart
@@ -0,0 +1,358 @@
+import 'package:flutter/material.dart';
+
+class AppScreen extends StatelessWidget {
+  const AppScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column( 
+        
+        children: [ 
+          
+          Row( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             
+              Container( 
+                decoration: BoxDecoration( 
+                  color: Colors.green,
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+                height: 50,
+                width: 450,
+                
+                
+              ),
+              SizedBox(width: 2
+              ,),
+               Container( 
+                decoration: BoxDecoration( 
+                  color: Colors.green,
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+                height: 50,
+                width: 450,
+                
+                
+              ),
+              
+            ],
+            
+          ),
+          
+          
+       Row(  mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          SizedBox(width: 3,),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          
+          
+
+          
+        ],
+       ),
+       SizedBox(
+        height: 5,
+       ),
+       Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 800,
+           
+          ),
+
+        ],
+       ),
+       Row(
+        children: [
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          Container(
+            decoration: BoxDecoration( 
+                  color: Color(0xffE473FF),
+                  boxShadow: [
+                    BoxShadow(
+                     color: Colors.black,
+                      
+                      blurRadius: (2),
+                      offset: Offset(2, 2)
+                      
+                      
+                    )
+                  ]
+                ),
+            height: 60,
+            width: 80,
+           
+          ),
+          
+
+        ],
+       )
+
+        ],
+        
+        
+    
+
+      ),
+      
+    );
+  }
+}
